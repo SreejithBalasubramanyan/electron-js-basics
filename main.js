30ad75da0553d74a8a88d7eb4d3d412d964a5d6f
sreejith_balasubramanyan <sbalasubramanyan@enablellc.io>
2022-10-19 23:17:10 +0530

    added menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron') // modified
 
 let mainWindow;
 const createMainWindow = () => {
@@ -6,7 +6,46 @@ const createMainWindow = () => {
     	width: 1500,
     	height: 1000
   	})
-  	 mainWindow.loadFile('mainWindow.html') // newly added
+  	 mainWindow.loadFile('mainWindow.html') 
 }
 
-app.on('ready', () => { createMainWindow() })
+//newly added
+
+let mainMenuHash = [ 
+	{ 
+		label: 'File', 
+		submenu: [ 
+			{
+				label: 'New',
+				accelerator: 'ctrl + shift + n',
+				click(){
+					console.log("need to trigger newItemWindow from here.")
+				}
+			},
+			{
+				type: 'separator'
+			},
+			{
+				role: 'quit'
+			},
+			
+		] 
+	},
+	{
+		label: 'view',
+		submenu:[
+			{
+				role: 'toggleDevTools',
+				label: 'open console'
+			}
+		]
+	}
+]
+
+//modified
+
+app.on('ready', () => { 
+	createMainWindow()
+	let mainMenu = Menu.buildFromTemplate(mainMenuHash)
+	Menu.setApplicationMenu(mainMenu) 
+})
