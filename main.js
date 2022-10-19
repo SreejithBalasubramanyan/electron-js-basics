a8e7d6e1a84865acdda15147fb2e87417e06c86d
sreejith_balasubramanyan <sbalasubramanyan@enablellc.io>
2022-10-19 23:08:46 +0530

    invoked main window from main process

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require('electron')
 
+let mainWindow;
 const createMainWindow = () => {
-  	const win = new BrowserWindow({
-    width: 1500,
-    height: 1000
-  })
+  	mainWindow = new BrowserWindow({
+    	width: 1500,
+    	height: 1000
+  	})
+  	 mainWindow.loadFile('mainWindow.html') // newly added
 }
 
 app.on('ready', () => { createMainWindow() })
